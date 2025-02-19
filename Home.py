596c60d09ab1d5cf1debf1208cf18ad8459f4951
Hemanth-Thaluru <thaluruhemanth105@gmail.com>
2025-02-18 22:09:01 -0500

    intial commit

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Langchain Chatbot",
+    page_icon='💬',
+    layout='wide'
+)
+
+st.header("Chatbot Implementations with Langchain And MongoDB")
+
+st.write("So, I’ve been working on this personal project lately—it’s something I’ve been really excited about.")
+st.write("It started with this idea I had, and I just wanted to see where I could take it.")
+st.write("I’ve been exploring different approaches, trying out new things, and honestly, it’s been pretty rewarding.")
+st.write("Still got a lot to figure out, but I’m learning a ton along the way.")
+st.write("It’s kind of my little passion project right now!")
